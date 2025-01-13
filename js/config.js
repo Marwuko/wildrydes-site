72af20970c9b27343551380dced0508d2f45d413
Marwuko <165482157+Marwuko@users.noreply.github.com>
2025-01-13 18:51:33 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'r0t3d8k93tjnre8hjo6cbf7oqhku87tgkseeudmb5i46b2b9bpf' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ler1hf6ha7.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
