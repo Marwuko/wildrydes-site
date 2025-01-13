a698b7cfbedbc308938c86ddc694fb3ed0bf31e7
Marwuko <165482157+Marwuko@users.noreply.github.com>
2025-01-13 16:51:23 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_p5sXPyWAH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '37qrcu7efs4733jdholnf3kdd5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_SjFdF3mJr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4fniruk83s7falq5ib7pit6l5d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
